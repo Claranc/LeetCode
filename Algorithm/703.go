1ab0d830b0803d632fd676a64ead925b33c48242
Claranc <fxj_bupt@163.com>
2018-10-22 21:53:44 +0800

    v1.0

--- Algorithm/703.go
+++ Algorithm/703.go
@@ -1,9 +1,9 @@
+//Solution1  用一个数组保存最大的k个值，每次更新后排序，输出第一个值
 type KthLargest struct {
     k int //输出第k大的数
     arr []int //保存最大的k个数
 }
 
-//用一个数组保存最大的k个值，每次更新后排序，输出第一个值
 func Constructor(k int, nums []int) KthLargest {
     sort.Ints(nums)
     if(len(nums) - k >= 0) { //能填满数组arr
@@ -33,5 +33,63 @@ func (this *KthLargest) Add(val int) int {
         this.arr[0] = val
         sort.Ints(this.arr)
         return this.arr[0]
+    }
+}
+
+//Solution 2: 使用min heap
+type IntHeap []int  // 定义一个类型
+
+func (h IntHeap) Len() int { return len(h) }  // 绑定len方法,返回长度
+
+func (h IntHeap) Less(i, j int) bool {  // 绑定less方法
+	return h[i] < h[j]  // 如果h[i]<h[j]生成的就是小根堆，如果h[i]>h[j]生成的就是大根堆
+}
+func (h IntHeap) Swap(i, j int) {  // 绑定swap方法，交换两个元素位置
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Pop() interface{} {  // 绑定pop方法，从最后拿出一个元素并返回
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func (h *IntHeap) Push(x interface{}) {  // 绑定push方法，插入新元素
+	*h = append(*h, x.(int))
+}
+
+type KthLargest struct {
+    k int //输出第k大的数
+    arr *IntHeap //总数为k个的min heap
+}
+
+
+func Constructor(k int, nums []int) KthLargest {
+    h := &IntHeap{} //建立空堆
+    heap.Init(h)
+    for _,v := range nums {
+        heap.Push(h, v) //将初始值添加进去
+    }
+    for h.Len() > k { //超出范围就把多的pop出来
+        heap.Pop(h)
+    }
+    return KthLargest{k, h}
+}
+
+func (this *KthLargest) Add(val int) int {
+    if this.arr.Len() < this.k {
+        heap.Push(this.arr, val)
+        if this.arr.Len() == this.k { 
+            return (*this.arr)[0]
+        } 
+    }
+    if val <= (*this.arr)[0] {
+        return (*this.arr)[0]
+    } else {
+        heap.Pop(this.arr)
+        heap.Push(this.arr, val)
+        return (*this.arr)[0]
     }
 }
